f80e00e006d34f5236274f80212f8dcd057001d8
Harikrishnan75704793 <harikrishnan10012003@gmail.com>
2025-03-07 10:25:42 +0530

    Initial commit

---
+++ frontend/src/pages/Doctors.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const Doctors = () => {
+
+  const { speciality } = useParams()
+
+  const [filterDoc, setFilterDoc] = useState([])
+  const [showFilter, setShowFilter] = useState(false)
+  const navigate = useNavigate();
+
+  const { doctors } = useContext(AppContext)
+
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter(doc => doc.speciality === speciality))
+    } else {
+      setFilterDoc(doctors)
+    }
+  }
+
+  useEffect(() => {
+    applyFilter()
+  }, [doctors, speciality])
+
+  return (
+    <div>
+      <p className='text-gray-600'>Browse through the doctors specialist.</p>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <button onClick={() => setShowFilter(!showFilter)} className={`py-1 px-3 border rounded text-sm  transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`}>Filters</button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
+          <p onClick={() => speciality === 'Endodontics' ? navigate('/doctors') : navigate('/doctors/Endodontics')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Endodontics' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Endodontics</p>
+          <p onClick={() => speciality === 'Orthodontics' ? navigate('/doctors') : navigate('/doctors/Orthodontics')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Orthodontics' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Orthodontics</p>
+          <p onClick={() => speciality === 'Periodontics' ? navigate('/doctors') : navigate('/doctors/Periodontics')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Periodontics' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Periodontics</p>
+          <p onClick={() => speciality === 'Prosthodontics' ? navigate('/doctors') : navigate('/doctors/Prosthodontics')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Prosthodontics' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Prosthodontics</p>
+          <p onClick={() => speciality === 'Maxillofacial' ? navigate('/doctors') : navigate('/doctors/Maxillofacial')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Maxillofacial' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Maxillofacial</p>
+          <p onClick={() => speciality === 'Pedodontics' ? navigate('/doctors') : navigate('/doctors/Pedodontics')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Pedodontics' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Pedodontics</p>
+        </div>
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
+          {filterDoc.map((item, index) => (
+            <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-[#C9D8FF] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+              <img className='bg-[#EAEFFF]' src={item.image} alt="" />
+              <div className='p-4'>
+                <div className={`flex items-center gap-2 text-sm text-center ${item.available ? 'text-green-500' : "text-gray-500"}`}>
+                  <p className={`w-2 h-2 rounded-full ${item.available ? 'bg-green-500' : "bg-gray-500"}`}></p><p>{item.available ? 'Available' : "Not Available"}</p>
+                </div>
+                <p className='text-[#262626] text-lg font-medium'>{item.name}</p>
+                <p className='text-[#5C5C5C] text-sm'>{item.speciality}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Doctors
